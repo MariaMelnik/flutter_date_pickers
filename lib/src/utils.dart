3c43b7641d105292b2ed5855297fe1ad46f54cba
Maria Melnik <maria@basservices.net>
2020-08-21 13:41:21 +0300

    Added useful functions to Utils.

--- lib/src/utils.dart
+++ lib/src/utils.dart
@@ -122,4 +122,90 @@ class DatePickerUtils {
   static DateTime startOfTheDay(DateTime date) {
     return DateTime(date.year, date.month, date.day);
   }
+
+  static DateTime firstShownDate(DateTime curMonth, bool showEndOfPrevMonth, int firstDayOfWeekFromSunday) {
+    DateTime result = DateTime(curMonth.year, curMonth.month, 1);
+
+    if (showEndOfPrevMonth) {
+      assert(firstDayOfWeekFromSunday != null);
+      int firstDayOffset = computeFirstDayOffset(curMonth.year, curMonth.month, firstDayOfWeekFromSunday);
+      if (firstDayOffset == 0) return null;
+
+
+      int prevMonth = curMonth.month - 1;
+      if (prevMonth < 1) prevMonth = 12;
+
+      int prevYear = prevMonth == 12
+        ? curMonth.year - 1
+        : curMonth.year;
+
+      int daysInPrevMonth = getDaysInMonth(prevYear, prevMonth);
+      int firstShownDay = daysInPrevMonth - firstDayOffset + 1;
+      result = DateTime(prevYear, prevMonth, firstShownDay);
+    }
+
+    return result;
+  }
+
+  static DateTime lastShownDate(DateTime curMonth, bool showStartNextMonth, int firstDayOfWeekFromSunday) {
+    int daysInCurMonth = getDaysInMonth(curMonth.year, curMonth.month);
+    DateTime result = DateTime(curMonth.year, curMonth.month, daysInCurMonth);
+
+    if (showStartNextMonth) {
+      assert(firstDayOfWeekFromSunday != null);
+      int firstDayOffset = computeFirstDayOffset(curMonth.year, curMonth.month, firstDayOfWeekFromSunday);
+
+      int totalDays = firstDayOffset + daysInCurMonth;
+      int trailingDaysCount = 7 - totalDays % 7;
+      bool fullWeekTrailing = trailingDaysCount == 7;
+      if (fullWeekTrailing) return null;
+
+      result = DateTime(curMonth.year, curMonth.month + 1, trailingDaysCount);
+    }
+
+    return result;
+  }
+
+  /// Computes the offset from the first day of week that the first day of the
+  /// [month] falls on.
+  ///
+  /// For example, September 1, 2017 falls on a Friday, which in the calendar
+  /// localized for United States English appears as:
+  ///
+  /// ```
+  /// S M T W T F S
+  /// _ _ _ _ _ 1 2
+  /// ```
+  ///
+  /// The offset for the first day of the months is the number of leading blanks
+  /// in the calendar, i.e. 5.
+  ///
+  /// The same date localized for the Russian calendar has a different offset,
+  /// because the first day of week is Monday rather than Sunday:
+  ///
+  /// ```
+  /// M T W T F S S
+  /// _ _ _ _ 1 2 3
+  /// ```
+  ///
+  /// So the offset is 4, rather than 5.
+  ///
+  /// This code consolidates the following:
+  ///
+  /// - [DateTime.weekday] provides a 1-based index into days of week, with 1
+  ///   falling on Monday.
+  /// - [MaterialLocalizations.firstDayOfWeekIndex] provides a 0-based index
+  ///   into the [MaterialLocalizations.narrowWeekdays] list.
+  /// - [MaterialLocalizations.narrowWeekdays] list provides localized names of
+  ///   days of week, always starting with Sunday and ending with Saturday.
+  static int computeFirstDayOffset(
+      int year, int month, int firstDayOfWeekFromSunday) {
+    // 0-based day of week, with 0 representing Monday.
+    final int weekdayFromMonday = DateTime(year, month).weekday - 1;
+    // firstDayOfWeekFromSunday recomputed to be Monday-based
+    final int firstDayOfWeekFromMonday = (firstDayOfWeekFromSunday - 1) % 7;
+    // Number of days between the first day of week appearing on the calendar,
+    // and the day corresponding to the 1-st of the month.
+    return (weekdayFromMonday - firstDayOfWeekFromMonday) % 7;
+  }
 }
